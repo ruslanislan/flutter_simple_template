0254c42e3ad0210a5c953d18749609d433e27c33
Ruslan Islanbekov <ruslan.islanbekov@dualbootpartners.com>
2023-03-07 05:09:19 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:go_router/go_router.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final preference = await SharedPreferences.getInstance();
+  runZonedGuarded(
+      () => runApp(
+            ScreenUtilInit(
+              designSize: const Size(390, 844),
+              builder: (BuildContext context, Widget? child) => MyApp(
+                preferences: preference,
+              ),
+            ),
+          ), (error, stack) {
+    debugPrint(error.toString());
+    debugPrintStack(stackTrace: stack);
+  });
+}
+
+CustomTransitionPage buildPageWithDefaultTransition({
+  required BuildContext context,
+  required GoRouterState state,
+  required Widget child,
+}) {
+  return CustomTransitionPage(
+    key: state.pageKey,
+    child: child,
+    transitionDuration: Duration.zero,
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      return FadeTransition(
+        opacity: animation,
+        child: child,
+      );
+    },
+  );
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({Key? key, required SharedPreferences preferences})
+      : super(key: key);
+
+  final GoRouter _router = GoRouter(routes: []);
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      routerConfig: _router,
+    );
+  }
+}
